a100744ac402cb193a3476c1e463161e4e14f7ab
Stas <nevmyvakastas@gmail.com>
2020-07-13 18:15:11 +0600

    created small server side

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
+
+const rooms = new Map([
+    
+]);
+
+app.get('/rooms', (req, res) => {
+    res.json(rooms)
+})
+
+io.on('connection', socket =>{
+    console.log('user connected', socket)
+})
+
+server.listen(9999, (err) =>{
+    if(err){
+        throw Error(err)
+    }
+    console.log('Server is running!')
+})
